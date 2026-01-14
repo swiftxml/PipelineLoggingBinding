047278b680f298da1dd5ef3a1e4dc9e8336c03a2
Stefan Springer <stefan.springer@icloud.com>
2026-01-14 08:16:10 +0100

    using Pipeline 1.0.4

--- Package.swift
+++ Package.swift
@@ -19,7 +19,7 @@ let package = Package(
         ),
     ],
     dependencies: [
-        .package(url: "https://github.com/swiftxml/Pipeline.git", from: "1.0.3"),
+        .package(url: "https://github.com/swiftxml/Pipeline.git", from: "1.0.4"),
         .package(url: "https://github.com/swiftxml/LoggingInterfaces.git", from: "1.0.0"),
     ],
     targets: [
